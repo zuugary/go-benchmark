57f1c98fb8957fb406189a555a214a4d25bd9da9
Gary Sentosa <gary.sentosa@zuuonline.com>
2023-07-20 17:21:56 +0900

    test: fix Fact2 tests

--- main_test.go
+++ main_test.go
@@ -7,15 +7,22 @@ import (
 )
 
 func benchmarkFact(b *testing.B, i int) {
+	// いろいろ重い処理
+	b.ResetTimer()
+
 	for n := 0; n < b.N; n++ {
 		ex.Fact(i)
+
+		b.StopTimer()
+		// 別の処理
+		b.StartTimer()
 	}
 }
 
-func BenchmarkFact_10(b *testing.B) { benchmarkFact(b, 10) }
-func BenchmarkFact_20(b *testing.B) { benchmarkFact(b, 20) }
-func BenchmarkFact_50(b *testing.B) { benchmarkFact(b, 50) }
-func BenchmarkFact_100(b *testing.B) { benchmarkFact(b, 100) }
+func BenchmarkFact_10(b *testing.B)   { benchmarkFact(b, 10) }
+func BenchmarkFact_20(b *testing.B)   { benchmarkFact(b, 20) }
+func BenchmarkFact_50(b *testing.B)   { benchmarkFact(b, 50) }
+func BenchmarkFact_100(b *testing.B)  { benchmarkFact(b, 100) }
 func BenchmarkFact_1000(b *testing.B) { benchmarkFact(b, 1000) }
 
 func benchmarkFact2(b *testing.B, i int) {
@@ -24,8 +31,8 @@ func benchmarkFact2(b *testing.B, i int) {
 	}
 }
 
-func BenchmarkFact2_10(b *testing.B) { benchmarkFact2(b, 10) }
-func BenchmarkFact2_20(b *testing.B) { benchmarkFact2(b, 10) }
-func BenchmarkFact2_50(b *testing.B) { benchmarkFact2(b, 10) }
-func BenchmarkFact2_100(b *testing.B) { benchmarkFact2(b, 10) }
+func BenchmarkFact2_10(b *testing.B)   { benchmarkFact2(b, 10) }
+func BenchmarkFact2_20(b *testing.B)   { benchmarkFact2(b, 20) }
+func BenchmarkFact2_50(b *testing.B)   { benchmarkFact2(b, 50) }
+func BenchmarkFact2_100(b *testing.B)  { benchmarkFact2(b, 100) }
 func BenchmarkFact2_1000(b *testing.B) { benchmarkFact2(b, 1000) }
